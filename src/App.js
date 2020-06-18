07e744326d9042922b3b96afd831ff79cfd5da60
Karen Lei <karenwinnielei@gmail.com>
2020-06-18 10:01:28 -0700

    change title, remove serverless function

--- src/App.js
+++ src/App.js
@@ -3,24 +3,15 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Web31 is _AMAZING_ </h1>
       <h2>
         Deployed with{' '}
         <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
+          href='https://vercel.com/docs'
+          target='_blank'
+          rel='noreferrer noopener'
         >
           Vercel
         </a>
@@ -28,28 +19,27 @@ function App() {
       </h2>
       <p>
         <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
+          href='https://github.com/vercel/vercel/tree/master/examples/create-react-app'
+          target='_blank'
+          rel='noreferrer noopener'
         >
           This project
         </a>{' '}
         was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
+        <a href='https://facebook.github.io/create-react-app/'>
           Create React App
         </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
+        and contains three directories, <code>/public</code> for
+        static assets, <code>/src</code> for components and content,
+        and <code>/api</code> which contains a serverless{' '}
+        <a href='https://golang.org/'>Go</a> function. See{' '}
+        <a href='/api/date'>
           <code>api/date</code> for the Date API with Go
         </a>
         .
       </p>
       <br />
       <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
     </main>
   );
 }
